6cb36e5dbc10042c528cd36d30aa06e3a4555f39
Errol Lin <errollin@qq.com>
2023-01-19 23:48:59 +0800

    Add expense type

---
+++ src/models/index.ts
@@ -0,0 +1,1 @@
+export type { Expense } from "@/models/interfaces/expense.interface";
---
+++ src/models/interfaces/expense.interface.ts
@@ -0,0 +1,6 @@
+export interface Expense {
+  id?: string | number;
+  title: string;
+  amount: number;
+  date: Date;
+}
